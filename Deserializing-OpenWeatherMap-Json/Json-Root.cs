4bb71711358bd43bf3e935095ad615db88ac7a53
johndward <johndward01@gmail.com>
2022-03-29 16:08:18 -0500

    init

---
+++ Deserializing-OpenWeatherMap-Json/Json-Root.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deserializing_OpenWeatherMap_Json
+{
+    class Json_Root
+    {
+        public List<Weather> WeatherList { get; set; } = new List<Weather>();
+    }
+}
---
+++ Deserializing-OpenWeatherMap-Json/Program.cs
@@ -0,0 +1,45 @@
+﻿using Deserializing_OpenWeatherMap_Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace OpenWeatherMap_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var jsonRootObject = new Json_Root();
+            
+            Console.Write("Please enter your API Key: ");
+            var api_Key = Console.ReadLine();
+
+            Console.WriteLine();
+            
+            Console.Write("Please enter the city name: ");
+            var city_name = Console.ReadLine();
+            
+            Console.WriteLine();
+
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&appid={api_Key}";
+            var response = client.GetStringAsync(weatherURL).Result;
+            var weatherJson = JObject.Parse(response).GetValue("weather").ToString();
+           
+            jsonRootObject.WeatherList = JsonConvert.DeserializeObject<List<Weather>>(weatherJson);
+            jsonRootObject.WeatherList.ForEach(x => PrintWeather(x));
+
+
+        }
+        
+        static void PrintWeather(Weather w)
+        {
+            Console.WriteLine($"Description: {w.description}");
+            Console.WriteLine(w.icon);
+            Console.WriteLine(w.id);
+            Console.WriteLine(w.main);
+        }
+    }
+}
